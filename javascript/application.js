c01a20553d741992e0f33b87b4ad2183e72e00b4
Stefan Smith <ssmith@thoughtworks.com>
2013-04-20 16:58:40 -0300

    Can put points with spherical coordinates on sphere

--- javascript/application.js
+++ javascript/application.js
@@ -14,23 +14,25 @@ $(function () {
         return particle;
     }
 
-    function CreateLineFromOrigin(materal, x, y, z) {
+    function CreateLineFromOrigin(material, x, y, z) {
         var geometry = new THREE.Geometry();
         geometry.vertices.push(new THREE.Vector3(0, 0, 0));
         geometry.vertices.push(new THREE.Vector3(x, y, z));
-        return new THREE.Line(geometry, materal);
+        return new THREE.Line(geometry, material);
     }
 
     function init() {
 
-        camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+//        camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+        camera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, -2000, 1000);
         camera.position.z = 500;
+        camera.position.y = 100;
 
         scene = new THREE.Scene();
 
         var universe = new THREE.Object3D();
-        celestialSphere = new THREE.SphereGeometry(200, 30, 30);
-        celestialSphereMaterial = new THREE.MeshNormalMaterial({ transparent: false, opacity: 0.25, color: 0xCC0000 });
+        celestialSphere = new THREE.SphereGeometry(200, 50, 50);
+        celestialSphereMaterial = new THREE.MeshNormalMaterial({ transparent: false, opacity: 0.75, color: 0xCC0000 });
 
         celestialSphereMesh = new THREE.Mesh(celestialSphere, celestialSphereMaterial);
         universe.add(celestialSphereMesh);
@@ -38,21 +40,30 @@ $(function () {
 
         celestialEquator = new THREE.CircleGeometry(200, 30);
         celestialEquator.vertices.splice(0, 1);
-        var lineBasicMaterial = new THREE.LineBasicMaterial({color: 0x000000});
+        var lineBasicMaterial = new THREE.LineBasicMaterial({color: 0x000000, linewidth: 5});
         equatorLine = new THREE.Line(celestialEquator, lineBasicMaterial);
+        equatorLine.rotation.x = 90 * Math.PI / 180;
 
         universe.add(equatorLine);
+        var vernalEquinoxMaterial = new THREE.LineBasicMaterial({color: 0x000000, linewidth: 10});
 
         var particleBasicMaterial = new THREE.ParticleBasicMaterial({size: 100, color: 0x000000});
 //        universe.add(CreateParticle(particleBasicMaterial, getX(200, 20, 45), getY(200, 20, 45), getZ(200, 20, 45)));
 //        universe.add(CreateParticle(particleBasicMaterial, getX(200, 0, 45), getY(200, 0, 45), getZ(200, 0, 45)));
+        universe.add(CreateLineFromOrigin(vernalEquinoxMaterial, getX(200, 0, 0), getY(200, 0, 0), getZ(200, 0, 0)));
+        universe.add(CreateLineFromOrigin(lineBasicMaterial, getX(200, 45, 0), getY(200, 45, 0), getZ(200, 45, 0)));
+        universe.add(CreateLineFromOrigin(lineBasicMaterial, getX(200, 0, 45), getY(200, 0, 45), getZ(200, 0, 45)));
         universe.add(CreateLineFromOrigin(lineBasicMaterial, getX(200, 45, 45), getY(200, 45, 45), getZ(200, 45, 45)));
+
+//        universe.add(CreateLineFromOrigin(lineBasicMaterial, 200, 0, 0));
+//        universe.add(CreateLineFromOrigin(lineBasicMaterial, 0, 200, 0));
+//        universe.add(CreateLineFromOrigin(lineBasicMaterial, 0, 0, 200));
+
+//        universe.add(CreateLineFromOrigin(lineBasicMaterial, getX(200, 0, -45), getY(200, 0, -45), getZ(200, 0, -45)));
 //        universe.add(CreateParticle(particleBasicMaterial, 0, 0, 100));
 
 
-//        equatorMesh.rotation.x = 85* Math.PI/180;
-
-        universe.rotation.x = 85 * Math.PI / 180;
+//        universe.rotation.x = 85 * Math.PI / 180;
 
 //        scene.add(equatorMesh);
         scene.add(universe);
@@ -64,22 +75,29 @@ $(function () {
 
     }
 
-    function getX(radius, phi, theta) {
-        return radius * Math.sin(phi) * Math.cos(theta);
+    function getX(radius, theta, phi) {
+        return radius * Math.cos(theta * Math.PI / 180) * Math.cos(phi * Math.PI / 180);
     }
 
-    function getY(radius, phi, theta) {
-        return radius * Math.sin(phi) * Math.sin(theta);
+    function getY(radius, theta, phi) {
+        return radius * Math.sin(phi * Math.PI / 180);
     }
 
-    function getZ(radius, phi, theta) {
-        return -radius * Math.cos(phi);
+    function getZ(radius, theta, phi) {
+        return -1 * radius * Math.sin(theta * Math.PI / 180) * Math.cos(phi * Math.PI / 180);
     }
 
     function animate() {
 
         // note: three.js includes requestAnimationFrame shim
-//        requestAnimationFrame(animate);
+        requestAnimationFrame(animate);
+
+        camera.lookAt(scene.position);
+
+        var timer = new Date().getTime() * 0.0005;
+
+        camera.position.x = Math.floor(Math.cos(timer) * 200);
+        camera.position.z = Math.floor(Math.sin(timer) * 200);
 //
 //        mesh.rotation.x += 0.01;
 //        mesh.rotation.y += 0.02;
